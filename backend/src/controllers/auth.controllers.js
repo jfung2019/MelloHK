281f1e614f0932f67d9efbd0364c1b4d838c23aa
jfung2019 <jfunglam@gmail.com>
2025-09-02 21:32:02 +0800

    move hashing of password on userModel everytime password is modified .e.g new user, and move password bcrypt checking in user Schema for encapsulation of logic (clear), Add email regex for login and signup, etc.

--- backend/src/controllers/auth.controllers.js
+++ backend/src/controllers/auth.controllers.js
@@ -6,35 +6,37 @@ import cloudinary from "../services/cloudinary.js";
 const signup = async (req, res) => {
   const { email, name, password } = req.body;
   try {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     if (!email || !name || !password) return res.status(400).json({ message: "Please fill up the required fields" });
+    if (!emailRegex.test(email)) return res.status(400).json({ message: "Invalid email format" });
     if (password.length < 6) return res.status(400).json({ message: "Password must be atleast 6 characters" });
     const user = await UserModel.findOne({ email });
-    if (user) return res.status(400).json({ message: "Email already exist!" });
+    if (user) return res.status(400).json({ message: "Email already exist, please use another email." });
+
+    // generate random profile picture as link for newly created user as default.
+    const idx = Math.floor(Math.random() * 100) + 1; // generate a num between 1-100
+    const randomAvatar = `https://avatar.iran.liara.run/public/${idx}.png`;
 
     // hash the password
-    const salt = await bcrypt.genSalt(10);
+    // const salt = await bcrypt.genSalt(10); moved to user model schema
     // convert password to non readable text e.g. 12345 -> wedgadw_123ghagd#12312
-    const hashedPassword = await bcrypt.hash(password, salt);
-    const newUser = new UserModel({
+    // const hashedPassword = await bcrypt.hash(password, salt); moved to user model schema
+    const newUser = await UserModel.create({
       name,
       email,
-      password: hashedPassword
+      password,
+      profilePicture: randomAvatar
     });
 
-    if (newUser) {
-      // generate jwt
-      generatetoken(newUser._id, res);
-      // save to database
-      await newUser.save();
-      return res.status(201).json({
-        _id: newUser._id,
-        name: newUser.name,
-        email: newUser.email,
-        profilePicture: newUser.profilePicture
-      });
-    } else {
-      return res.status(400).json({ message: "Invalid user data" });
-    }
+    // create new user for stream for getstreamio
+    // generate jwt, add token and cookie to the response
+    generatetoken(newUser._id, res);
+    // save to database
+    // await newUser.save();
+    return res.status(201).json({
+      success: true,
+      user: newUser
+    });
   } catch (err) {
     console.log("error in sign up controller", err.message)
     res.status(500).json({ message: "Internal Server Error" });
@@ -42,15 +44,15 @@ const signup = async (req, res) => {
 }
 
 const login = async (req, res) => {
-  // TODO: Implement login logic
   const { email, password } = req.body;
-  if (!email || !password) {
-    return res.status(400).json({ message: "Email and/or password are required" });
-  }
+  if (!email || !password) return res.status(400).json({ message: "Email and/or password are required" });
   try {
     const user = await UserModel.findOne({ email });
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
     if (!user) return res.status(400).json({ message: "Invalid credentials" });
-    const isPassWordValid = await bcrypt.compare(password, user.password);
+    if (!emailRegex.test(email)) return res.status(400).json({ message: "Invalid email format" });
+    const isPassWordValid = await user.isMatchPassword(password);
     if (!isPassWordValid || password.length < 6) return res.status(400).json({ message: "Invalid credentials" });
     generatetoken(user._id, res);
     res.status(200).json({
@@ -103,6 +105,7 @@ const checkAuth = (req, res) => {
   }
 }
 
+// add token and cookie to the response
 const generatetoken = (userId, res) => {
   const token = jwt.sign({ userId }, process.env.JWT_SECRET_KEY, { expiresIn: "7d" });
   console.log("res", res);
@@ -110,10 +113,9 @@ const generatetoken = (userId, res) => {
     maxAge: 7 * 24 * 60 * 60 * 1000, //ms
     httpOnly: true, // prevent XSS attacks cross-site scripting attacks
     sameSite: "strict", // CSRF attacks cross-site request forgery attacks
-    secure: process.env.NODE_ENV !== "development"
+    secure: process.env.NODE_ENV === "production"
   });
   console.log("res token", token);
-  return token;
 }
 
 export { login, logout, signup, profileUpdate, checkAuth };
